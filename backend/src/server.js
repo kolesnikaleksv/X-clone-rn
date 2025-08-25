a3b2e31e1253153da4841ff793f054030a2ca99b
kolesnikaleksv <kolesnikaleksv@gmail.com>
2025-08-25 12:55:17 +0300

    Add vercel config

--- backend/src/server.js
+++ backend/src/server.js
@@ -36,9 +36,12 @@ const startServer = async () => {
   try {
     await connectDB();
 
-    app.listen(ENV.PORT, () =>
-      console.log(`Server is up, and runing on PORT: ${ENV.PORT}`)
-    );
+    // listen for local development
+    if (ENV.NODE_ENV !== 'production') {
+      app.listen(ENV.PORT, () =>
+        console.log(`Server is up, and runing on PORT: ${ENV.PORT}`)
+      );
+    }
   } catch (error) {
     console.error('Failed to start server', error);
     process.exit(1);
@@ -46,3 +49,6 @@ const startServer = async () => {
 };
 
 startServer();
+
+// export for vercel
+export default app;
