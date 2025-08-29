93477dbc1ce805b3145fb6ff7ee0ba06e23a7680
kolesnikaleksv <kolesnikaleksv@gmail.com>
2025-08-29 18:14:21 +0300

    Add lastname fiels to user model schema

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -16,6 +16,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    lastName: {
+      type: String,
+      required: true,
+    },
     username: {
       type: String,
       required: true,
